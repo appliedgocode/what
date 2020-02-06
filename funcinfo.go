333b988152105003b9a3ecb93426e13ea1303e34
christophberger <christophberger@users.noreply.github.com>
2020-02-06 07:37:15 +0100

    Formatting

--- funcinfo.go
+++ funcinfo.go
@@ -4,7 +4,7 @@ package what
 
 import "runtime"
 
-// funcinfo returns func name, line, and file name of a 
+// funcinfo returns func name, line, and file name of a
 // caller, after skipping skip callers.
 func funcinfo(skip int) (name, file string, line int) {
 	pc, file, line, ok := runtime.Caller(skip)
@@ -14,9 +14,9 @@ func funcinfo(skip int) (name, file string, line int) {
 	return name, file, line
 }
 
-// funcname returns the name of a 
+// funcname returns the name of a
 // caller, after skipping skip callers.
 func funcname(skip int) string {
-	name, _, _ := funcinfo(skip+1)
+	name, _, _ := funcinfo(skip + 1)
 	return name
 }
